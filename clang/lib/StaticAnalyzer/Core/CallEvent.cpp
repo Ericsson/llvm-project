b1ecb1c77b749c55a6447a142b02298e545de618
Donát Nagy <donat.nagy@ericsson.com>
2026-03-17 13:38:27 +0100

    Revert "[NFCI][analyzer] Fix logic in getReturnValueUnderConstruction"
    This reverts commit a17505ee08717c354fb327731b15ec43466237f9 because it
    will be split off into a separate PR.

--- clang/lib/StaticAnalyzer/Core/CallEvent.cpp
+++ clang/lib/StaticAnalyzer/Core/CallEvent.cpp
@@ -562,11 +562,13 @@ std::optional<SVal> CallEvent::getReturnValueUnderConstruction() const {
 
   EvalCallOptions CallOpts;
   ExprEngine &Engine = getState()->getStateManager().getOwningEngine();
-  unsigned NumVisitedCall = Engine.getNumVisited(
-      getLocationContext(), getCFGElementRef().getParent());
+  // FIXME: This code assumes that the _current_ location context and block is
+  // the location and block where this `CallExpr` is called. For a more stable
+  // solution `Engine.getNumVisitedCurrent()` should be replaced with a call to
+  // `Engine.getNumVisited(<CallerLCtx>, <CallerBlock>)`.
   SVal RetVal = Engine.computeObjectUnderConstruction(
-      getOriginExpr(), getState(), NumVisitedCall, getLocationContext(), CC,
-      CallOpts);
+      getOriginExpr(), getState(), Engine.getNumVisitedCurrent(),
+      getLocationContext(), CC, CallOpts);
   return RetVal;
 }
 
