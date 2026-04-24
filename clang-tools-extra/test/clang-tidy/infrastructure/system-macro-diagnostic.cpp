70c4c1c2c2e524f48696fed4c3bd63a76e72ebf6
Donát Nagy <donat.nagy@ericsson.com>
2026-04-24 11:05:26 +0200

    [NFC] Validate that disabling 'clang-diagnostic-*' still works

--- clang-tools-extra/test/clang-tidy/infrastructure/system-macro-diagnostic.cpp
+++ clang-tools-extra/test/clang-tidy/infrastructure/system-macro-diagnostic.cpp
@@ -1,5 +1,10 @@
 // RUN: clang-tidy -checks='-*,clang-diagnostic-invalid-offsetof,concurrency-mt-unsafe' -header-filter='.*' -system-headers=true %s -- -isystem %S/Inputs/system-headers 2>&1 | FileCheck -check-prefix=CHECK-SYSTEM-HEADERS %s
 // RUN: clang-tidy -checks='-*,clang-diagnostic-invalid-offsetof,concurrency-mt-unsafe' -header-filter='.*' -system-headers=false %s -- -isystem %S/Inputs/system-headers 2>&1 | FileCheck -check-prefix=CHECK-NO-SYSTEM-HEADERS %s
+
+// Validate that we don't get the diagnostics when the 'clang-diagnostic-*'
+// check is disabled:
+// RUN: clang-tidy -checks='-*,concurrency-mt-unsafe' -header-filter='.*' -system-headers=true %s -- -isystem %S/Inputs/system-headers 2>&1 | FileCheck -implicit-check-not='{{warning:|error:}}' %s
+
 // FIXME: The check 'concurrency-mt-unsafe' is completely unrelated to this
 // test, it is only added to the RUN lines because Clang-Tidy aborts the
 // analysis with "Error: no checks enabled." if all the enabled checks are
