dc838c3cf01adcc4f435d630a8e6c605b1906a34
Donát Nagy <donat.nagy@ericsson.com>
2026-04-24 17:08:32 +0200

    [NFC] Remove builder in VisitMemberExpr
    This change is NFC becasue the two `generateNode` calls and the
    `takeNodes` call are together sufficient to remove each element `I` of
    `CheckedSet` from `EvalSet` (where the constructor of `NodeBuilder`
    inserts them initially).
    
    Also notice that in this method we need to explicitly pass `state` to
    `makeNodeWithBinding` because the `createTemporaryRegionIfNeeded` calls
    may modify the state before the final `BindExpr` (which is now
    incorporated into `makeNodeWithBinding`).

--- clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
+++ clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
@@ -3511,7 +3511,6 @@ void ExprEngine::VisitMemberExpr(const MemberExpr *M, ExplodedNode *Pred,
     for (const auto I : CheckedSet)
       VisitCommonDeclRefExpr(M, Member, I, EvalSet);
   } else {
-    NodeBuilder Bldr(CheckedSet, EvalSet, *currBldrCtx);
     ExplodedNodeSet Tmp;
 
     for (const auto I : CheckedSet) {
@@ -3525,9 +3524,8 @@ void ExprEngine::VisitMemberExpr(const MemberExpr *M, ExplodedNode *Pred,
           state = createTemporaryRegionIfNeeded(state, LCtx, BaseExpr);
 
         SVal MDVal = svalBuilder.getFunctionPointer(MD);
-        state = state->BindExpr(M, LCtx, MDVal);
 
-        Bldr.generateNode(M, I, state);
+        EvalSet.insert(Engine.makeNodeWithBinding(I, M, MDVal, state));
         continue;
       }
 
@@ -3571,12 +3569,11 @@ void ExprEngine::VisitMemberExpr(const MemberExpr *M, ExplodedNode *Pred,
             L = UnknownVal();
         }
 
-        Bldr.generateNode(M, I, state->BindExpr(M, LCtx, L), nullptr,
-                          ProgramPoint::PostLValueKind);
+        EvalSet.insert(Engine.makeNodeWithBinding(
+            I, M, L, state, ProgramPoint::PostLValueKind));
       } else {
-        Bldr.takeNodes(I);
         evalLoad(Tmp, M, M, I, state, L);
-        Bldr.addNodes(Tmp);
+        EvalSet.insert(Tmp);
       }
     }
   }
